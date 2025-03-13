7730dc811c733f7f66a2708d1c84d6dcd2fd0455
Just-Lou <143757336+Just-Lou@users.noreply.github.com>
2025-03-13 13:10:39 -0400

    Threadcutter shit

--- QtMENnette/Views/ThreadCutterView.h
+++ QtMENnette/Views/ThreadCutterView.h
@@ -1,8 +1,18 @@
 #pragma once
 #include "ThreadCutterModel.h"
+#include "ui_MainWindow.h"
+#include <QWidget>
+#include <qlabel.h>
+#include <qfont.h>
 
-class ThreadCutterView
+class ThreadCutterView : public QWidget
 {
+	Q_OBJECT
 public:
+	ThreadCutterView(QWidget* parent = nullptr);
 	void render(const ThreadCutterModel& model);
+	void failedModule();
+
+private:
+	Ui::MainWindow* ui;
 };
