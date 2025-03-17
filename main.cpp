be4eb0783302d63be377279a45a81339134190e7
ZwoukLeBleu <88751650+ZwoukLeBleu@users.noreply.github.com>
2025-03-17 15:24:30 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -25,31 +25,24 @@
 #define MSG_SIZE 9
 #define MSG_ID_FROM_ARDUINO 142
 #define MSG_ID_FROM_PC 242
+#define MSG_ID_ERROR 71
 
+struct Frame { 
+  uint8_t id;
+  uint8_t ledState;
+  uint8_t potentiometer;
+  uint8_t joystickX;
+  uint8_t joystickY;
+  uint8_t accelerometerX;
+  uint8_t accelerometerY;
+  uint8_t accelerometerZ;
+  uint8_t checksum;
+};
 
-
-uint8_t inc = 0;    
-bool state_LED1 = false;
-bool state_LED2 = false;
-bool state_LED3 = false;
-bool state_LED4 = false;
-bool state_SW2 = false;
-bool state_SW3 = false;
-bool state_SW4 = false;
-bool state_SW5 = false;
-
-uint8_t value_pot = 0;
-uint8_t value_jstickX = 0;
-uint8_t value_jstickY = 0;
-uint8_t value_accelX = 0;
-uint8_t value_accelY = 0;
-uint8_t value_accelZ = 0;
-
-
-
+Frame errorFrame = {MSG_ID_ERROR, 0, 0, 0, 0, 0, 0, 0, 0};
 
 void sendMsg(); 
-void readMsg();
+Frame readMsg();
 void serialEvent();
 byte convertBoolsToByte(bool p_sw[8]);
 void byteToBoolArray(byte b, bool arr[8]);
@@ -63,16 +56,22 @@ void setup() {
 }
 
 void loop() {
-  readMsg();
+  // * Code de base! Modifiez a votre guise. 
+  // Cependant, faite en sorte que vous envoyez des message a une frequence maximal de la clock du programme PC + 1ms
+  // Sinon, il y a un backlog de message qui se cree dans le buffer, et un ping substantiel se manifeste
+  // 2025-03-17: clock_pc=33ms --> lowest=delay(34)
+  
+  Frame frame = readMsg();
   sendMsg();
   delay(50);  
 }
 
 
 void sendMsg() {
-  while (Serial.available()) {
-    Serial.read();
-  }
+  // * Si le code bug et que le buffer reste rempli, decommenter les 3 lignes suivantes
+  // while (Serial.available()) {
+  //   Serial.read();
+  // }
   
   //Boutons
   bool sw[8] = { digitalRead(PIN_LED_1),
@@ -84,8 +83,6 @@ void sendMsg() {
                   digitalRead(PIN_SW_4),
                   digitalRead(PIN_SW_5) };
   byte b1 = convertBoolsToByte(sw);
-  // byte b1 = inc++;
-  if (inc > 255) inc = 0;
   byte pot = uint8_t(analogRead(PIN_POT)/4); // 0-1023 -> 0-255
   byte jstickX = uint8_t(analogRead(PIN_JSTICK_X)/4);
   byte jstickY = uint8_t(analogRead(PIN_JSTICK_Y)/4);
@@ -93,13 +90,8 @@ void sendMsg() {
   byte accelY = uint8_t(analogRead(PIN_ACCEL_Y)/4);
   byte accelZ = uint8_t(analogRead(PIN_ACCEL_Z)/4);
   
+  //Creation de message
   byte msg[MSG_SIZE] = {MSG_ID_FROM_ARDUINO, b1, pot, jstickX, jstickY, accelX, accelY, accelZ, 0};
-  // byte msg[MSG_SIZE] = {142, 11, 22, 33, 44, 0, 0, 0, 0};
-  // bool sw2[8] = {state_LED1, state_LED2, state_LED3, state_LED4, state_SW2, state_SW3, state_SW4, state_SW5};
-  // byte b2 = convertBoolsToByte(sw2);
-
-  // byte msg[MSG_SIZE] = {142, b2, value_pot, value_jstickX, value_jstickY, value_accelX, value_accelY, value_accelZ, 0};
-  
   // Checksum via XOR de tous les bytes
   byte checksum = 0;
   for (int i = 0; i < MSG_SIZE-1; i++) {
@@ -114,68 +106,34 @@ void sendMsg() {
 }
 
 
-void readMsg(){
+Frame readMsg(){
   byte msg[MSG_SIZE];
   if (Serial.available() < MSG_SIZE) { 
-    // Serial.println("WARING: Not enough bytes available! Message ignored");
-    return; 
+    Serial.println("WARING-AR: Not enough bytes available! Message ignored");
+    return errorFrame;  
   }
   int a = Serial.readBytes(msg, MSG_SIZE);
   if (a != MSG_SIZE) { 
-    // Serial.println("WARING: Not enough bytes read! Message ignored");
-    return; 
+    Serial.println("WARING-AR: Not enough bytes read! Message ignored");
+    return errorFrame; 
   }
   if (msg[0] != MSG_ID_FROM_PC) { 
-    // Serial.println("WARING: Invalid message! Message ignored");
-    return; 
+    Serial.println("WARING-AR: Invalid message! Message ignored");
+    return errorFrame; 
   }
  
-
   // Checksum via XOR de tous les bytes
   byte checksum = 0;
   for (int i = 0; i < MSG_SIZE - 1; i++) {
     checksum ^= msg[i];
   }
   if (checksum != msg[MSG_SIZE - 1]) { 
-    // Serial.println("WARING: Checksum error! Message ignored");
-    return; 
+    Serial.println("WARING-AR: Checksum error! Message ignored");
+    return errorFrame;  
     }
 
-  bool sw[8];
-  byteToBoolArray(msg[1], sw);
-  byte pot = msg[2];
-  byte jstickX = msg[3];
-  byte jstickY = msg[4];
-  byte accelX = msg[5];
-  byte accelY = msg[6];
-  byte accelZ = msg[7];
-  
-  // digitalWrite(LED_BUILTIN, ledState);
-  // ledState = !ledState;
-  if (state_LED1 != sw[0]) {
-    state_LED1 = sw[0];
-    digitalWrite(PIN_LED_1, state_LED1 ? HIGH : LOW);
-  }
-  if (state_LED2 != sw[1]) {
-    state_LED2 = sw[1];
-    digitalWrite(PIN_LED_2, state_LED2 ? HIGH : LOW);
-  }
-  state_LED1 = sw[0];
-  state_LED2 = sw[1];
-  state_LED3 = sw[2];
-  state_LED4 = sw[3];
-  state_SW2 = sw[4];
-  state_SW3 = sw[5];
-  state_SW4 = sw[6];
-  state_SW5 = sw[7];
-
-  value_pot = uint8_t(pot);
-  value_jstickX = uint8_t(jstickX);
-  value_jstickY = uint8_t(jstickY);
-  value_accelX = uint8_t(accelX);
-  value_accelY = uint8_t(accelY);
-  value_accelZ = uint8_t(accelZ);
-  
+  Frame frame = {msg[0], msg[1], msg[2], msg[3], msg[4], msg[5], msg[6], msg[7], msg[8]};
+  return frame;
 }
 
 byte convertBoolsToByte(bool p_sw[8]) {
