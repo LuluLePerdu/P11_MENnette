d906e486ada0cce561a5f75ca8920cdf5279fce0
ZwoukLeBleu <88751650+ZwoukLeBleu@users.noreply.github.com>
2025-03-18 14:27:49 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -21,38 +21,38 @@
 #define PIN_LED_4 28
 #define PIN_POT A15
 
-#define BAUD 9600       
-#define MSG_SIZE 9
-#define MSG_ID_FROM_ARDUINO 142
-#define MSG_ID_FROM_PC 242
+#define BAUD 115200      
+#define MSG_SIZE 3
 #define MSG_ID_ERROR 71
+
+#define MSG_ID_JOYSTICK 101
+#define MSG_ID_POTENTIOMETER 102
+#define MSG_ID_ACCELEROMETER 103
+#define MSG_ID_BUTTON 104
+
+#define MSG_ID_PC_LED 201
+#define MSG_ID_PC_MOTOR 202
+#define MSG_ID_PC_LCD 203
+#define MSG_ID_PC_STATE 204
+#define MSG_ID_FROM_MISC 242
 
 struct Frame { 
   uint8_t id;
-  uint8_t ledState;
-  uint8_t potentiometer;
-  uint8_t joystickX;
-  uint8_t joystickY;
-  uint8_t accelerometerX;
-  uint8_t accelerometerY;
-  uint8_t accelerometerZ;
+  uint8_t data;
   uint8_t checksum;
 };
 
-Frame errorFrame = {MSG_ID_ERROR, 0, 0, 0, 0, 0, 0, 0, 0};
+Frame errorFrame = {MSG_ID_ERROR, 0, 0};
 
-void sendMsg(); 
+void sendMsg(Frame frame); 
 Frame readMsg();
+Frame readMsg(byte id);
 void serialEvent();
 byte convertBoolsToByte(bool p_sw[8]);
 void byteToBoolArray(byte b, bool arr[8]);
 
 void setup() {
   Serial.begin(BAUD);              
-  pinMode(LED_BUILTIN, OUTPUT);    
-  pinMode(PIN_LED_1, OUTPUT);
-  pinMode(PIN_LED_2, OUTPUT);
-  pinMode(PIN_POT, INPUT);
 }
 
 void loop() {
@@ -61,37 +61,19 @@ void loop() {
   // Sinon, il y a un backlog de message qui se cree dans le buffer, et un ping substantiel se manifeste
   // 2025-03-17: clock_pc=33ms --> lowest=delay(34)
   
+  sendMsg(errorFrame);
   Frame frame = readMsg();
-  sendMsg();
   delay(50);  
 }
 
-
-void sendMsg() {
+void sendMsg(Frame frame) {
   // * Si le code bug et que le buffer reste rempli, decommenter les 3 lignes suivantes
   // while (Serial.available()) {
   //   Serial.read();
   // }
   
-  //Boutons
-  bool sw[8] = { digitalRead(PIN_LED_1),
-                  digitalRead(PIN_LED_2),
-                  digitalRead(PIN_LED_3),
-                  digitalRead(PIN_LED_4),
-                  digitalRead(PIN_SW_2),
-                  digitalRead(PIN_SW_3),
-                  digitalRead(PIN_SW_4),
-                  digitalRead(PIN_SW_5) };
-  byte b1 = convertBoolsToByte(sw);
-  byte pot = uint8_t(analogRead(PIN_POT)/4); // 0-1023 -> 0-255
-  byte jstickX = uint8_t(analogRead(PIN_JSTICK_X)/4);
-  byte jstickY = uint8_t(analogRead(PIN_JSTICK_Y)/4);
-  byte accelX = uint8_t(analogRead(PIN_ACCEL_X)/4);
-  byte accelY = uint8_t(analogRead(PIN_ACCEL_Y)/4);
-  byte accelZ = uint8_t(analogRead(PIN_ACCEL_Z)/4);
-  
   //Creation de message
-  byte msg[MSG_SIZE] = {MSG_ID_FROM_ARDUINO, b1, pot, jstickX, jstickY, accelX, accelY, accelZ, 0};
+  byte msg[MSG_SIZE] = {frame.id, frame.data, frame.checksum};
   // Checksum via XOR de tous les bytes
   byte checksum = 0;
   for (int i = 0; i < MSG_SIZE-1; i++) {
@@ -117,7 +99,37 @@ Frame readMsg(){
     Serial.println("WARING-AR: Not enough bytes read! Message ignored");
     return errorFrame; 
   }
-  if (msg[0] != MSG_ID_FROM_PC) { 
+  // if (msg[0] != MSG_ID_FROM_PC) { 
+  //   Serial.println("WARING-AR: Invalid message! Message ignored");
+  //   return errorFrame; 
+  // }
+ 
+  // Checksum via XOR de tous les bytes
+  byte checksum = 0;
+  for (int i = 0; i < MSG_SIZE - 1; i++) {
+    checksum ^= msg[i];
+  }
+  if (checksum != msg[MSG_SIZE - 1]) { 
+    Serial.println("WARING-AR: Checksum error! Message ignored");
+    return errorFrame;  
+    }
+
+  Frame frame = {msg[0], msg[1], msg[2]};
+  return frame;
+}
+
+Frame readMsg(byte id){
+  byte msg[MSG_SIZE];
+  if (Serial.available() < MSG_SIZE) { 
+    Serial.println("WARING-AR: Not enough bytes available! Message ignored");
+    return errorFrame;  
+  }
+  int a = Serial.readBytes(msg, MSG_SIZE);
+  if (a != MSG_SIZE) { 
+    Serial.println("WARING-AR: Not enough bytes read! Message ignored");
+    return errorFrame; 
+  }
+  if (msg[0] != id) { 
     Serial.println("WARING-AR: Invalid message! Message ignored");
     return errorFrame; 
   }
@@ -132,7 +144,7 @@ Frame readMsg(){
     return errorFrame;  
     }
 
-  Frame frame = {msg[0], msg[1], msg[2], msg[3], msg[4], msg[5], msg[6], msg[7], msg[8]};
+  Frame frame = {msg[0], msg[1], msg[2]};
   return frame;
 }
 
