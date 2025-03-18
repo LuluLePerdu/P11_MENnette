d498fac682b744df3fc45d5929790f09a5fbd3e3
ZwoukLeBleu <88751650+ZwoukLeBleu@users.noreply.github.com>
2025-03-18 14:28:07 -0400

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -13,28 +13,35 @@
 
 using namespace std;
 
-#define BAUD 9600       
-#define MSG_SIZE 9
-#define MSG_ID_FROM_ARDUINO 142
-#define MSG_ID_FROM_PC 242
+#define BAUD 115200       
+#define MSG_SIZE 3
+
 #define MSG_ID_ERROR 71
+
+#define MSG_ID_JOYSTICK 101
+#define MSG_ID_POTENTIOMETER 102
+#define MSG_ID_ACCELEROMETER 103
+#define MSG_ID_BUTTON 104
+
+#define MSG_ID_PC_LED 201
+#define MSG_ID_PC_MOTOR 202
+#define MSG_ID_PC_LCD 203
+#define MSG_ID_PC_STATE 204
+#define MSG_ID_FROM_MISC 242
+
+
 
 struct Frame { 
     uint8_t id;
-    uint8_t ledState;
-    uint8_t potentiometer;
-    uint8_t joystickX;
-    uint8_t joystickY;
-    uint8_t accelerometerX;
-    uint8_t accelerometerY;
-    uint8_t accelerometerZ;
+    uint8_t data;
     uint8_t checksum;
 };
 
-Frame errorFrame = {MSG_ID_ERROR, 0, 0, 0, 0, 0, 0, 0, 0};
+Frame errorFrame = {MSG_ID_ERROR, 0, 0};
 
 void sendMsg(HANDLE hSerial, Frame frame);
 Frame readMsg(HANDLE hSerial);
+Frame readMsg(HANDLE hSerial, uint8_t startByte);
 bool configureSerialPort(HANDLE hSerial);
 
 int main() {
@@ -55,11 +62,11 @@ int main() {
         CloseHandle(hSerial);
         return -1;
     }
-    Frame frame = {MSG_ID_FROM_PC, 255, 2, 3, 4, 5, 6, 7, 0};
+    Frame frame = {MSG_ID_PC_LED, 255, 0};
 
     while (true) {
-        sendMsg(hSerial, frame);
-        Frame test = readMsg(hSerial);
+        sendMsg(hSerial, errorFrame);
+        Frame test = readMsg(hSerial, MSG_ID_ERROR);
         this_thread::sleep_for(chrono::milliseconds(33));
     }
 
@@ -90,7 +97,7 @@ bool configureSerialPort(HANDLE hSerial) {
 
 void sendMsg(HANDLE hSerial, Frame frame) {
     // Creation de message
-    uint8_t msg[MSG_SIZE] = {frame.id, frame.ledState, frame.potentiometer, frame.joystickX, frame.joystickY, frame.accelerometerX, frame.accelerometerY, frame.accelerometerZ, 0};
+    uint8_t msg[MSG_SIZE] = {frame.id, frame.data, 0};
 
     // Checksum via XOR de tous les bytes
     uint8_t checksum = 0;
@@ -113,8 +120,9 @@ void sendMsg(HANDLE hSerial, Frame frame) {
 
 Frame readMsg(HANDLE hSerial) {
     uint8_t msg[MSG_SIZE] = {0}; 
+    Frame frame;
     DWORD bytesRead;
-    uint8_t startByte = MSG_ID_FROM_ARDUINO;
+    uint8_t startByte = msg[0];
 
     // Synchronize avec le debut du message MSG_ID_FROM_ARDUINO
     while (true) {
@@ -147,6 +155,45 @@ Frame readMsg(HANDLE hSerial) {
         cout << setw(3) << (int)msg[i] << " ";
     }
     cout << endl;
-    Frame frame = {msg[0], msg[1], msg[2], msg[3], msg[4], msg[5], msg[6], msg[7], msg[8]};
+    frame = {msg[0], msg[1], msg[2]};
+    return frame;
+}
+
+Frame readMsg(HANDLE hSerial, uint8_t startByte) {
+    uint8_t msg[MSG_SIZE] = {0}; 
+    DWORD bytesRead;
+
+    // Synchronize avec le debut du message MSG_ID_FROM_ARDUINO
+    while (true) {
+        if (!ReadFile(hSerial, &msg[0], 1, &bytesRead, NULL) || bytesRead < 1) {
+            cerr << "WARNING-PC: Not enough bytes read! Message ignored" << endl;
+            return errorFrame;
+        }
+        if (msg[0] == startByte) {
+            break;
+        }
+    }
+
+    // Lecture 
+    if (!ReadFile(hSerial, &msg[1], MSG_SIZE - 1, &bytesRead, NULL) || bytesRead < MSG_SIZE - 1) {
+        cerr << "WARNING-PC: Not enough bytes read! Message ignored" << endl;
+        return errorFrame;
+    }
+
+    uint8_t checksum = 0;
+    for (int i = 0; i < MSG_SIZE - 1; i++) {
+        checksum ^= msg[i];
+    }
+    if (checksum != msg[MSG_SIZE - 1]) {
+        cerr << "WARNING-PC: Checksum error! Message ignored" << endl;
+        return errorFrame;
+    }
+
+    cout << "LOG-PC: Received message: ";
+    for (int i = 0; i < MSG_SIZE; i++) {
+        cout << setw(3) << (int)msg[i] << " ";
+    }
+    cout << endl;
+    Frame frame = {msg[0], msg[1], msg[2]};
     return frame;
 }
