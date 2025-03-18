002584da2936ef0a20114ac72cba64dffb928586
Just-Lou <143757336+Just-Lou@users.noreply.github.com>
2025-03-18 16:33:23 -0400

    Ajout fonction potentiometre pas opti

---
+++ MENnette/MENnette/PotentiometreALL.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <random>
+using namespace std;
+
+bool playPot() {
+    char characters[] = "!@±/£$¢%¤?¬&¦*()=+#|\\*-[]^¨<>}{`;:,.'";
+    char digits[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+    int target = (rand() % 255);
+    cout << "Target : " << target << endl;
+    char code[10];
+    for (int i = 0; i < 10; i++) {
+        code[i] = digits[rand() % 9];
+    }
+
+    bool isOver = false;
+    float usrInput, distance;
+    char tempArray[10];
+
+    while (!isOver) {
+        cout << "Entrez un nombre (0-255) : ";
+        cin >> usrInput;
+        distance = abs(target - usrInput);
+        int distanceTen = 10 * distance / 128;
+        if (distanceTen > 10) distanceTen = 10;
+        cout << "Base 10 distance : " << distanceTen << endl;
+        if (distanceTen == 0) {
+            isOver = true;
+        }
+        else {
+            cout << "nbchar : " << distanceTen << endl;
+            for (int i = 0; i < distanceTen; i++) {
+                tempArray[i] = characters[rand() % 44];
+            }
+            for (int i = distanceTen; i < 10; i++) {
+                tempArray[i] = code[i];
+            }
+            random_shuffle(begin(tempArray), end(tempArray));
+            for (int i = 0; i < 10; i++) {
+                cout << tempArray[i];
+            }
+            cout << endl;
+            system("pause");
+        }
+
+        system("cls");
+    }
+
+    return true;
+
+}
