713eb0adfa683e6848367b52fd31fb5eaf8a6e3f
Just-Lou <143757336+Just-Lou@users.noreply.github.com>
2025-03-27 15:47:25 -0400

    Created SimonSays files

---
+++ QtMENnette/SimonSays.cpp
@@ -0,0 +1,1 @@
+#include "SimonSays.h"
---
+++ QtMENnette/SimonSays.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <QWidget>
+#include <qlabel.h>
+#include "MainWindow.h"
+#include "Communication.h"
+
+class SimonSays : public QWidget
+{
+public:
+	SimonSays();
+	
+private:
+
+};
