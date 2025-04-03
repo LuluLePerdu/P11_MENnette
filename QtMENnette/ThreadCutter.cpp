6647d12a69c499b47727004cd78149331be00b07
Just-Lou <143757336+Just-Lou@users.noreply.github.com>
2025-04-03 15:37:39 -0400

    les led allument allegedly

---
+++ QtMENnette/ThreadCutter.cpp
@@ -0,0 +1,104 @@
+#include "ThreadCutter.h"
+#include <iostream>
+using namespace std;
+
+ThreadCutter::ThreadCutter() {
+    srand(time(0));
+    for (int i = 0; i < 4; i++) {
+        ledArray[i] = rand() % 2;
+    }
+    ledSetup();
+    render();
+}
+
+void ThreadCutter::run() {
+    bool ledArray[8];
+    Communication& comm = Communication::getInstance();
+
+    int msgInput;
+    comm.clear();
+
+    do
+    {
+        msgInput = comm.readMsg(MSG_ID_AR_BUTTON);
+        comm.clear();
+        if (msgInput == -2) {
+            return;
+        }
+    } while (msgInput == -1);
+
+    switch (msgInput)
+    {
+    case 1:
+        checkButton(3);
+        break;
+    case 2:
+        checkButton(2);
+        break;
+    case 4:
+        checkButton(1);
+        break;
+    case 8:
+        checkButton(0);
+        break;
+    default:
+        break;
+    }
+}
+
+bool ThreadCutter::checkButton(int button) {
+    if (button == correctLed) completed = true;
+	return completed;
+}
+
+void ThreadCutter::render() {
+    Communication& comm = Communication::getInstance();
+    Frame msgLED;
+    msgLED.id = MSG_ID_PC_LED;
+    bool delStates[8] = { ledArray[0], ledArray[1], ledArray[2], ledArray[3], 0, 0, 0, 0};
+    uint8_t byteMsg = comm.convertBoolsToByte(delStates);
+    msgLED.data = byteMsg;
+    comm.sendMsg(msgLED);
+}
+
+bool ThreadCutter::getCompleted() {
+    return completed;
+}
+
+void ThreadCutter::ledSetup() {
+    /* old code (fonctionne mais moins complet et clair)
+    if (ledArray[0] && ledArray[1] + ledArray[2] + ledArray[3] == 2)
+    {
+        correctLed = 1;
+    }
+    else if ((ledArray[0] && ledArray[2] && !ledArray[1] && !ledArray[3])
+        || (ledArray[0] && ledArray[1] && ledArray[2] && ledArray[3])) {
+        correctLed = 3;
+    }
+    else if (!ledArray[0] && !ledArray[1] && !ledArray[2] && !ledArray[3]) {
+        correctLed = 4;
+    }
+    else {
+        correctLed = 2;
+    }
+    */
+    if ((ledArray[GREEN] && ledArray[BLUE] + ledArray[RED] + ledArray[YELLOW] == 2)
+        || (!ledArray[GREEN] && !ledArray[BLUE] && ledArray[RED] && !ledArray[YELLOW]))
+    {
+        correctLed = GREEN;
+    }
+    else if ((ledArray[GREEN] && ledArray[RED] && !ledArray[BLUE] && !ledArray[YELLOW])
+        || (ledArray[GREEN] && ledArray[BLUE] && ledArray[RED] && ledArray[YELLOW])) {
+        correctLed = RED;
+    }
+    else if ((!ledArray[GREEN] && !ledArray[BLUE] && !ledArray[RED] && !ledArray[YELLOW])
+        || (ledArray[BLUE] && ledArray[YELLOW] && !ledArray[GREEN] && !ledArray[RED])) {
+        correctLed = YELLOW;
+    }
+    else if (ledArray[GREEN] + ledArray[BLUE] + ledArray[RED] + ledArray[YELLOW] == 2) {
+        correctLed = RED;
+    }
+    else {
+        correctLed = BLUE;
+    }
+}
